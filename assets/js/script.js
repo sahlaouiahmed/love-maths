5903164e503b1bba0b2ac64e1dc931e4754fda4b
sahlaouiahmed <sahlaouiahmad@outlook.com>
2024-08-17 14:46:04 +0000

    division function

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,7 +42,10 @@ function runGame(gameType) {
 		displayMultiplyQuestion(num1, num2);
 	} else if (gameType === "subtract") {
 		displaySubtractQuestion(num1, num2);
-	} else {
+	} else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
+    }
+    else{
 		alert(`Unknown game type ${gameType}`);
 		throw `Unknown game type ${gameType}, aborting!`;
 	}
@@ -85,7 +88,9 @@ function calculateCorrectAnswer() {
 		return [operand1 * operand2, "multiply"];
 	} else if (operator ==="-") {
 		return [operand1 - operand2, "subtract"];
-	} else {
+	} else if( operator ==="/") {
+        return [operand1 / operand2, "division"]
+    } else {
 		alert(`Unimplemented operator ${operator}`);
 		throw `Unimplemented operator ${operator}, aborting!`;
 	}
@@ -133,4 +138,8 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 }
 
-// Add your division question here
+function displayDivisionQuestion (operand1, operand2){
+	document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 * operand2 : operand2 * operand1 ;
+	document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+	document.getElementById("operator").textContent = "/";
+}
